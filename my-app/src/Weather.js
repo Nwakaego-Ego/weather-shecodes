cf435bfdef8faf5c79200133ee23d390fa94194b
Nwakaego-Ego <81041326+Nwakaego-Ego@users.noreply.github.com>
2021-12-22 17:44:52 +0100

    Update Weather.js file

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function Weather() {
     return (
         <div className="Weather">
-            <h1>Hey</h1>
+            <form>
+                <input type="text" placeholder="Enter in city..."/>
+            </form>
         </div>
     )
 }
